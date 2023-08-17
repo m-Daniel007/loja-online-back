e9e19bdc0ba9b35b3955c42b2145752e5bea0cfd
Marcos <marcosdanieldossantos@yahoo.com.br>
2023-08-17 17:53:01 -0300

    feat: :sparkles: criacao de categoriasService

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { Roles } from '../decorators/roles.decorators';
+import { UserType } from '../user/enum/userType.unum';
+import { ReturnCategory } from './dto/returnCategory.dto';
 
+@Roles(UserType.Admin, UserType.User)
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  async findAllCategories(): Promise<ReturnCategory[]> {
+    return (await this.categoryService.findAllCategories()).map(
+      (category) => new ReturnCategory(category),
+    );
+  }
 }
