6c623906e7ecdc80937787b161c2606038af9ca1
Marcos <marcosdanieldossantos@yahoo.com.br>
2023-09-07 00:00:15 -0300

    feat: :sparkles: serviço de insert cart products

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
-import { Roles } from 'decorators/roles.decorators';
-import { UserType } from 'user/enum/userType.unum';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CartEntity } from './entities/cart.entity';
+import { UserType } from '../user/enum/userType.unum';
+import { Roles } from '../decorators/roles.decorators';
+import { CartService } from './cart.service';
+import { InsertCartDTO } from './dto/insertCart.dto';
+import { UserId } from '../decorators/userId.decorator';
 
-@Roles(UserType.User)
+@Roles(UserType.User, UserType.Admin)
 @Controller('cart')
-export class CartController {}
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @Post()
+  async createCart(
+    @Body() insertCart: InsertCartDTO,
+    @UserId() userId: number,
+  ): Promise<CartEntity> {
+    return this.cartService.insertProductInCartService(insertCart, userId);
+  }
+}
