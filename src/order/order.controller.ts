b338d34a6164af26796177c1613368068987ae6b
Marcos <marcosdanieldossantos@yahoo.com.br>
2023-10-31 12:34:44 -0300

    feat: :sparkles: criacao do controller de order

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { CreateOrderDto } from './dto/createOrder.dto';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Post('/cart/:cartId')
+  async createOrder(
+    @Body() createOrder: CreateOrderDto,
+    @Param('cartId') cartId: number,
+  ) {
+    return this.orderService.createOrderService(createOrder, cartId);
+  }
+}
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { OrderEntity } from './entities/order.entity';
+import { CreateOrderDto } from './dto/createOrder.dto';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  constructor(
+    @InjectRepository(OrderEntity)
+    private readonly orderEntity: Repository<OrderEntity>,
+  ) {}
+
+  async createOrderService(createOrder: CreateOrderDto, cartId: number) {
+    return null;
+  }
+}
